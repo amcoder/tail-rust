ca208d08f39ae3a0f08dc45edbf20e6c86ab8eb3
Andrew Miller <amcoder@gmail.com>
2014-10-13 14:05:59 -0400

    Refactor option parsing to a separate method

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,27 @@ use std::io::{
     SeekSet, SeekCur, SeekEnd,
 };
 use std::cmp::{ min };
-use getopts::{ optflag, optopt, getopts, usage };
+use getopts::{ optflag, optopt, getopts, usage, OptGroup };
 
 static VERSION: &'static str = "0.0.1";
 
 static BUFFER_SIZE: uint = 1024;
 static DEFAULT_LINES: uint = 10;
 
+// Represents the set of options
+struct TailOptions {
+    show_help: bool,
+    show_version: bool,
+    output_headers: bool,
+    item_count: uint,
+    files: Vec<String>,
+}
+
 fn main() {
     let args = args();
     let program = args[0].clone();
 
-    let options = [
+    let possible_options = [
         optopt("n", "lines", "output the last K lines", "K"),
         optflag("q", "quiet", "never output file name headers"),
         optflag("", "silent", "same as --quiet"),
@@ -29,61 +38,76 @@ fn main() {
         optflag("V", "version", "display version information and exit"),
     ];
 
-    let option_matches = match getopts(args.tail(), options) {
-        Ok(m) => m,
+    let options = match parse_options(args.tail(), possible_options) {
+        Ok(o) => o,
         Err(error) => {
             (writeln!(stderr(), "{}: {}", program, error.to_string())).unwrap();
             return;
         },
     };
 
-    if option_matches.opt_present("h") {
+    if options.show_help {
         let brief = format!("Usage: {} [OPTION]... [FILE]...", program);
-        println!("{}", usage(brief.as_slice(), options));
+        println!("{}", usage(brief.as_slice(), possible_options));
         return;
     }
 
-    if option_matches.opt_present("version") {
+    if options.show_version {
         println!("tail-rust v{}", VERSION);
         return;
     }
 
-    let line_count = match option_matches.opt_str("lines") {
-        Some(nstr) => {
-            match from_str(nstr.as_slice()) {
-                Some(n) => n,
-                None => {
-                    (writeln!(stderr(), "{}: {}: invalid number of lines",
-                                program, nstr)).unwrap();
-                    return;
-                },
-            }
-        },
-        None => DEFAULT_LINES,
-    };
-
-    let files = &option_matches.free;
-    let output_headers = !option_matches.opt_present("quiet") &&
-                            !option_matches.opt_present("silent") &&
-                            (option_matches.opt_present("verbose") ||
-                             files.len() > 1);
-
-    for file_name in files.iter() {
+    for file_name in options.files.iter() {
 
         // Output the header, but only if we are tailing more than one file
-        if output_headers {
+        if options.output_headers {
             println!("==> {} <==", file_name);
         }
 
         // Open the file and tail it
         match File::open(&Path::new(file_name.as_slice()))
-                    .and_then(|f| { tail_file(f, line_count) }) {
+                    .and_then(|f| { tail_file(f, options.item_count) }) {
             Err(error) => {
                 (writeln!(stderr(), "{}: {}: {}", program, file_name, error.desc)).unwrap();
             },
             _ => continue,
         }
     }
+}
+
+// Given a set of arguments and possible options, parse the arguments and
+// return the selected TailOptions
+fn parse_options(args: &[String],
+                 options: &[OptGroup]) -> Result<TailOptions, String> {
+
+    let option_matches = match getopts(args, options) {
+        Ok(o) => o,
+        Err(error) => return Err(error.to_string()),
+    };
+
+    let options = TailOptions {
+        show_help: option_matches.opt_present("help"),
+        show_version: option_matches.opt_present("version"),
+        output_headers: !option_matches.opt_present("quiet") &&
+                            !option_matches.opt_present("silent") &&
+                            (option_matches.opt_present("verbose") ||
+                             option_matches.free.len() > 1),
+        item_count: match option_matches.opt_str("lines") {
+            Some(nstr) => {
+                match from_str(nstr.as_slice()) {
+                    Some(n) => n,
+                    None => {
+                        return Err(format!("{}: invalid number of lines",
+                                           nstr));
+                    },
+                }
+            },
+            None => DEFAULT_LINES,
+        },
+        files: option_matches.free,
+    };
+
+    return Ok(options);
 }
 
 // Output the last 'n' lines from 'file'
